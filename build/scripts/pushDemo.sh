72795588d97feb650a783ead9a730e40fbf976a9
Oceanxy <xyzsyx@163.com>
2019-01-22 11:41:29 +0800

    Modify CI configuration

---
+++ build/scripts/pushDemo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git remote remove origin git@github.com:oceanxy/react-tabllist.git
+git remote add github git@github.com:oceanxy/react-tabllist.git
+git remote add coding git@git.dev.tencent.com:Oceanxy/react-tabllist.git
+
+yarn demo && yarn push-demo
+
