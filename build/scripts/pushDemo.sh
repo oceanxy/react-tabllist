e22cbc533b21c1be84522bc08e40ce6915b023fc
Oceanxy <xyzsyx@163.com>
2019-01-22 14:59:32 +0800

    Modify CI configuration

--- build/scripts/pushDemo.sh
+++ build/scripts/pushDemo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 git remote remove origin git@github.com:oceanxy/react-tabllist.git
-git remote add github https://oceanxy:GH_TOKEN@github.com/oceanxy/react-tabllist.git
-git remote add coding https://oceanxy:CODING_TOKEN@git.dev.tencent.com/Oceanxy/react-tabllist.git
+git remote add github "https://${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
+git remote add coding "https://${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
 
 yarn demo && yarn push-demo
 
