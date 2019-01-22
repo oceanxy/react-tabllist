95f7f579df82e7f6bc5eed16000118ab4fe8756e
Oceanxy <xyzsyx@163.com>
2019-01-22 18:20:44 +0800

    write test cases

--- build/scripts/pushDemo.sh
+++ build/scripts/pushDemo.sh
@@ -4,6 +4,7 @@ git remote remove origin git@github.com:oceanxy/react-tabllist.git
 git remote add github "https://${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
 git remote add coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
 
+git fetch
 git pull github master
 
 yarn demo && yarn push-demo
