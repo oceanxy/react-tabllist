0206ce45c12a9563dff0648163897bd61d48aceb
Oceanxy <xyzsyx@163.com>
2019-01-23 10:43:49 +0800

    modify git url of pushDemo.sh

--- build/scripts/pushDemo.sh
+++ build/scripts/pushDemo.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-git remote remove origin "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
-git remote add github "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
-git remote add coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
+git remote remove origin "https://github.com/oceanxy/react-tabllist.git"
+
+git remote add -f github "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
+git remote add -f coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
+
+git subtree add -P examples github gh-pages --squash
+git subtree add -P examples github coding-pages --squash
+
+git fetch github master
+git fetch coding master
+git subtree pull -P github gh-pages --squash
+git subtree pull -P coding coding-pages --squash
 
 yarn demo && yarn push-demo
