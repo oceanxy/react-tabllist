01184cf47a368fc5a878ab4ed65e4a51c786ac17
Oceanxy <xyzsyx@163.com>
2019-01-23 16:41:02 +0800

    test travis CI

--- build/scripts/pushDemo.sh
+++ build/scripts/pushDemo.sh
@@ -15,8 +15,8 @@ yarn demo
 git remote rm origin
 git remote add github "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
 git remote add coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
+
 git remote -v
 
-git pull github master
-
-yarn push-demo
+git push github `git subtree split --prefix examples gh-pages`:production --force
+git push coding `git subtree split --prefix examples coding-pages`:production --force
