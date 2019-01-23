18be1fdc16ae8f1a745a4e97901e8c7542d0105b
Oceanxy <xyzsyx@163.com>
2019-01-23 15:22:26 +0800

    test travis CI

--- build/scripts/pushDemo.sh
+++ build/scripts/pushDemo.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-git remote remove origin "https://github.com/oceanxy/react-tabllist.git"
+#git remote remove origin "https://github.com/oceanxy/react-tabllist.git"
+#
+#git remote add -f github "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
+#git remote add coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
+#
+#yarn demo
+#
+#git subtree add -P examples github gh-pages --squash
+#
+#git fetch github gh-pages
+#
+#git pull github master
+#
+#git subtree pull -P examples github gh-pages --squash
 
-git remote add -f github "https://oceanxy:${GH_TOKEN}@github.com/oceanxy/react-tabllist.git"
-git remote add coding "https://oceanxy:${CODING_TOKEN}@git.dev.tencent.com/Oceanxy/react-tabllist.git"
-
-yarn demo
-
-git subtree add -P examples github gh-pages --squash
-
-git fetch github gh-pages
-
-git pull github master
-
-git subtree pull -P examples github gh-pages --squash
-
-yarn push-demo
+yarn demo && yarn push-demo
