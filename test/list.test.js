24a1296e47a813a445444d24bc23b8252b551b2d
Oceanxy <xyzsyx@163.com>
2019-01-26 16:39:53 +0800

    Split dev branch

--- test/list.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactTabllist from '../src/list'
-import Enzyme from './enzyme.config'
-
-it('exports modules correctly', () => {
-  expect(ReactTabllist).toMatchSnapshot()
-})
