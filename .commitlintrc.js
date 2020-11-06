c9ce50745f83d1600f712888aad2c3e252cfe2a7
Oceanxy <xyzsyx@163.com>
2020-11-06 10:30:38 +0800

    anno: optimize the comments of .commitlintrc.js

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,24 +1,35 @@
-/**
- * build : 改变了build工具 如 webpack
- * ci : 持续集成新增
- * chore : 构建过程或辅助工具的变动
- * feat : 新功能
- * docs : 文档改变
- * fix : 修复bug
- * perf : 性能优化
- * refactor : 某个已有功能重构
- * revert : 撤销上一次的 commit
- * style : 代码格式改变
- * test : 增加测试
- * anno: 增加注释
- */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
       'always',
-      ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'anno']
+      [
+        // 改变了build工具 如 webpack
+        'build',
+        // 持续集成新增
+        'ci',
+        // 构建过程或辅助工具的变动
+        'chore',
+        // 文档改变
+        'docs',
+        // 新功能
+        'feat',
+        // 修复bug
+        'fix',
+        // 性能优化
+        'perf',
+        // 某个已有功能重构
+        'refactor',
+        // 撤销上一次的 commit
+        'revert',
+        // 代码格式改变
+        'style',
+        // 增加测试
+        'test',
+        // 增加注释
+        'anno'
+      ]
     ]
   }
 }
