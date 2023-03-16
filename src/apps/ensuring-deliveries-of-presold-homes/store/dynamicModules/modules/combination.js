7f59d4135394ef3ace6053cc7cddb0effa51c6c2
张桂苓 <zhgling@lanjor.cn>
2023-03-16 17:56:16 +0800

    删除treeIdField多余字段

--- src/apps/ensuring-deliveries-of-presold-homes/store/dynamicModules/modules/combination.js
+++ src/apps/ensuring-deliveries-of-presold-homes/store/dynamicModules/modules/combination.js
@@ -4,7 +4,6 @@ export default commitRootInModule =>
   createStoreModule(
     {
       state: {
-        treeIdField: '',
         organTree: {
           loading: false,
           list: []
--- src/apps/ensuring-deliveries-of-presold-homes/store/dynamicModules/modules/menus.js
+++ src/apps/ensuring-deliveries-of-presold-homes/store/dynamicModules/modules/menus.js
@@ -3,7 +3,6 @@ import { createStoreModule } from '@/store/template'
 export default commitRootInModule =>
   createStoreModule({
     state: {
-      treeIdField: '',
       menuTree: {
         loading: false,
         list: []
