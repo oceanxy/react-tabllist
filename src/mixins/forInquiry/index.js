1ad2ae154e1c2b9e3a93c47e32f7099f0df1c6dc
Omsber <xyzsyx@163.com>
2023-08-11 14:47:58 +0800

    feat: forInquiry混合新增setParams和setOptions参数

--- src/mixins/forInquiry/index.js
+++ src/mixins/forInquiry/index.js
@@ -1,7 +1,6 @@
 /**
  * 表格搜索 混合
- * @Author: Omsber
- * @Email: xyzsyx@163.com
+ * @Author: Omsber <xyzsyx@163.com>
  * @Date: 2022-03-14 周一 15:33:20
  */
 
@@ -10,58 +9,181 @@ import moment from 'moment'
 import { Button, Form, Space } from 'ant-design-vue'
 
 /**
- * 生成用于表格搜索的混合
- * @param [isFetchList=true] {boolean} 是否通过本组件的搜索按钮来请求数据，默认 true。设置为 false 时，将隐藏搜索按钮。
- *  在一些特殊场景，搜索按钮只负责改变 store 内的值，例如：
- *  如果其他组件（如左侧树、页面列表等）有请求数据的逻辑，此处请设置为 false，搜索按钮仅仅用来控制 store.state.search 的值，
- *  发送请求的逻辑交由这些组件来完成。
- * @param [isInitializeFromStore=false] {boolean} 在组件加载成功时，是否把 store 内对应本组件的模块的搜索参数映射到 Form 组件内，默认 false
- * @param [buttonDisabled] {() => boolean} 禁用查询按钮的方法
- * @returns {Object}
+ * 用于表格搜索的混合
+ * @mixin
+ * @param {boolean} [isFetchList=true] 是否通过本组件的搜索按钮来请求数据。
+ * - 设置为 false 时，将隐藏搜索按钮。
+ * - 在一些特殊场景，搜索按钮只负责改变 store 内的值，例如：
+ * 如果其他组件（如左侧树、页面列表等）有请求数据的逻辑，此处请设置为 false，此时搜索按钮仅仅用来控制 store.state.search 的值，将发送请求的逻辑交由这些组件来完成。
+ * @param {boolean} [isInitializeFromStore=false] 在组件加载成功时，是否把 store 内对应本组件的模块的搜索参数映射到 Form 组件内。
+ * @param {function():boolean} [buttonDisabledFn] 禁用查询按钮的方法。
+ * @param {function():Object} [setParams={}] 调用查询接口时需要的额外请求参数。一般用于配置在子模块内的 inquiry 组件获取父模块参数等。
+ * @param {function():Object} [setOptions={}] 调用查询接口时需要的`全局Action(setSearch、getList)`配置。一般用于自定义请求接口等配置。
+ * @returns {Object<Vue.mixin>}
  */
-export default ({
+export default function forInquiry({
   isFetchList = true,
   isInitializeFromStore = false,
-  buttonDisabledFn
-} = {}) => ({
-  inject: {
-    moduleName: { default: '' },
-    submoduleName: { default: '' },
-    /**
-     * 注入树标识：判断当前组件是否启用侧边树
-     * 来自于 @/components/TGContainerWithTreeSider
-     */
-    inTree: { default: false },
-    /**
-     * 注入弹窗标识：判断当前组件是否在弹窗内
-     * 来自于 @/mixins/forModal
-     */
-    inModal: { default: false }
-  },
-  data() {
-    return {
-      // 为了缓存 onSubmit 的参数
-      params: {},
-      // 为了缓存 onSubmit 的参数
-      options: {},
-      // 搜索表单初始化值
-      initialValues: {},
-      // 按钮禁用状态
-      buttonDisabled: false
-    }
-  },
-  computed: {
-    search: {
-      get() {
-        if (this.submoduleName) {
-          return this.$store.state[this.moduleName][this.submoduleName].search
+  buttonDisabledFn,
+  setParams,
+  setOptions
+} = {}) {
+  return {
+    inject: {
+      moduleName: { default: '' },
+      submoduleName: { default: '' },
+      /**
+       * 注入树标识：判断当前组件是否启用侧边树
+       * 来自于 @/components/TGContainerWithTreeSider
+       */
+      inTree: { default: false },
+      /**
+       * 注入弹窗标识：判断当前组件是否在弹窗内
+       * 来自于 @/mixins/forModal
+       */
+      inModal: { default: false }
+    },
+    data() {
+      return {
+        // 为了缓存 onSubmit 的参数
+        params: {},
+        // 为了缓存 onSubmit 的参数
+        options: {},
+        // 搜索表单初始化值
+        initialValues: {},
+        // 按钮禁用状态
+        buttonDisabled: false
+      }
+    },
+    computed: {
+      search: {
+        get() {
+          if (this.submoduleName) {
+            return this.$store.state[this.moduleName][this.submoduleName].search
+          }
+
+          return this.$store.state[this.moduleName].search
+        },
+        set(value) {
+          if (Object.keys(value || {}).length) {
+            this.$store.commit('setState', {
+              value: this.transformValue(value),
+              moduleName: this.moduleName,
+              submoduleName: this.submoduleName,
+              stateName: 'search',
+              merge: true
+            })
+          }
+        }
+      },
+      treeCollapsed: {
+        get() {
+          return this.$store.state['common'].treeCollapsed
+        },
+        async set(value) {
+          this.$store.commit('setState', {
+            value,
+            moduleName: 'common',
+            stateName: 'treeCollapsed'
+          })
+        }
+      },
+      sideToggle() {
+        return (
+          <div class={'tg-inquiry-side-toggle'}>
+            <IconFont
+              class={'tree-btn'}
+              type={'icon-side-tree-toggle'}
+              onClick={this.onTreeFold}
+              title={'折叠/展开 左侧树'}
+            />
+          </div>
+        )
+      },
+      operationButtons() {
+        return (
+          <Space class={'tg-inquiry-form-buttons'}>
+            <Button
+              disabled={this.buttonDisabled}
+              loading={this.loading}
+              htmlType="submit"
+              type="primary"
+              icon="search"
+            >
+              查询
+            </Button>
+            <Button
+              onClick={this.onClear}
+              icon="reload"
+            >
+              重置并刷新
+            </Button>
+          </Space>
+        )
+      },
+      content() {
+        let content = this.forRender
+
+        if (!content) {
+          console.error(`未检测到 ${this.moduleName}${this.submoduleName
+            ? `.${this.submoduleName}`
+            : ''
+          } 内 Inquiry 组件的 forRender 计算属性，请确认！`)
+
+          return undefined
         }
 
-        return this.$store.state[this.moduleName].search
-      },
-      set(value) {
-        if (Object.keys(value || {}).length) {
-          this.$store.commit('setState', {
+        // 将渲染内容统一转换为数组以便后续处理
+        if (!Array.isArray(content)) {
+          content = [content]
+        }
+
+        // 当渲染数组中至少存在一个表单项时，将其内容全部放进 .inquiry-row-for-fields 容器
+        if (content.find(VNode => VNode?.tag?.includes('AFormItem'))) {
+          content = [
+            <div class={'inquiry-row-for-fields'}>
+              {content}
+            </div>
+          ]
+        }
+
+        if (this.inTree && !this.inModal && !content[0]?.children[0]?.data?.class?.includes('tg-inquiry-side-toggle')) {
+          content.at(0)?.children?.unshift(this.sideToggle)
+        }
+
+        if (isFetchList) {
+          content.at(-1)?.children?.push(this.operationButtons)
+        }
+
+        return content
+      }
+    },
+    created() {
+      if (typeof buttonDisabledFn === 'function') {
+        this.$watch(
+          () => this.form?.getFieldsValue(),
+          () => {
+            this.buttonDisabled = buttonDisabledFn.call(this)
+
+            this.$nextTick(() => {
+              // 仅仅为了使用 Form 组件的检验功能来改变必填框的错误状态
+              this.form.validateFields(() => {/*写一个空函数，不然控制台会有错误信息输出*/})
+            })
+          }
+        )
+      }
+
+      // 同步 store.state.search 与 混入组件中定义的 initialValues，
+      // 根据初始值的来源，可自行选择在混入组件的 computed 或 data 中定义 initialValues 对象
+      if (isInitializeFromStore) {
+        this.initialValues = { ...this.initialValues, ...cloneDeep(this.search) }
+      }
+
+      // 监听搜索表单的值的变化，与 store 做同步。以便其他组件执行表格查询时的参数统一
+      this.$watch(
+        () => this.form.getFieldsValue(),
+        async value => {
+          await this.$store.commit('setState', {
             value: this.transformValue(value),
             moduleName: this.moduleName,
             submoduleName: this.submoduleName,
@@ -69,231 +191,130 @@ export default ({
             merge: true
           })
         }
+      )
+
+      this.search = this.initialValues
+    },
+    mounted() {
+      if (!isInitializeFromStore) {
+        this.search = this.convertBoolean(cloneDeep(this.form.getFieldsValue()))
       }
     },
-    treeCollapsed: {
-      get() {
-        return this.$store.state['common'].treeCollapsed
+    methods: {
+      convertBoolean(value) {
+        const temp = {}
+
+        Object.entries(value).forEach(([k, v]) => {
+          if (isBoolean(v)) {
+            temp[k] = v ? 1 : 0
+          } else {
+            temp[k] = v
+          }
+        }, {})
+
+        return temp
       },
-      async set(value) {
-        this.$store.commit('setState', {
-          value,
-          moduleName: 'common',
-          stateName: 'treeCollapsed'
-        })
-      }
-    },
-    sideToggle() {
-      return (
-        <div class={'tg-inquiry-side-toggle'}>
-          <IconFont
-            class={'tree-btn'}
-            type={'icon-side-tree-toggle'}
-            onClick={this.onTreeFold}
-            title={'折叠/展开 左侧树'}
-          />
-        </div>
-      )
-    },
-    operationButtons() {
-      return (
-        <Space class={'tg-inquiry-form-buttons'}>
-          <Button
-            disabled={this.buttonDisabled}
-            loading={this.loading}
-            htmlType="submit"
-            type="primary"
-            icon="search"
-          >
-            查询
-          </Button>
-          <Button
-            onClick={this.onClear}
-            icon="reload"
-          >
-            重置并刷新
-          </Button>
-        </Space>
-      )
-    },
-    content() {
-      let content = this.forRender
+      /**
+       * 此函数值保留一些高频共用类参数的处理
+       * @param values
+       * @returns {{}}
+       */
+      transformValue(values) {
+        let temp = this.convertBoolean(cloneDeep(values))
 
-      if (!content) {
-        console.error(`未检测到 ${this.moduleName}${this.submoduleName
-          ? `.${this.submoduleName}`
-          : ''
-        } 内 Inquiry 组件的 forRender 计算属性，请确认！`)
+        if ('dateRange' in temp) {
+          temp.startTime = temp.dateRange[0] ? moment(temp.dateRange[0]).format('YYYYMMDD') : ''
+          temp.endTime = temp.dateRange[1] ? moment(temp.dateRange[1]).format('YYYYMMDD') : ''
 
-        return undefined
-      }
-
-      // 将渲染内容统一转换为数组以便后续处理
-      if (!Array.isArray(content)) {
-        content = [content]
-      }
-
-      // 当渲染数组中至少存在一个表单项时，将其内容全部放进 .inquiry-row-for-fields 容器
-      if (content.find(VNode => VNode?.tag?.includes('AFormItem'))) {
-        content = [
-          <div class={'inquiry-row-for-fields'}>
-            {content}
-          </div>
-        ]
-      }
-
-      if (this.inTree && !this.inModal && !content[0]?.children[0]?.data?.class?.includes('tg-inquiry-side-toggle')) {
-        content.at(0)?.children?.unshift(this.sideToggle)
-      }
-
-      if (isFetchList) {
-        content.at(-1)?.children?.push(this.operationButtons)
-      }
-
-      return content
-    }
-  },
-  created() {
-    if (typeof buttonDisabledFn === 'function') {
-      this.$watch(
-        () => this.form?.getFieldsValue(),
-        () => {
-          this.buttonDisabled = buttonDisabledFn.call(this)
-
-          this.$nextTick(() => {
-            // 仅仅为了使用 Form 组件的检验功能来改变必填框的错误状态
-            this.form.validateFields(() => {/*写一个空函数，不然控制台会有错误信息输出*/})
-          })
+          temp = omit(temp, 'dateRange')
         }
-      )
-    }
 
-    // 同步 store.state.search 与 混入组件中定义的 initialValues，
-    // 根据初始值的来源，可自行选择在混入组件的 computed 或 data 中定义 initialValues 对象
-    if (isInitializeFromStore) {
-      this.initialValues = { ...this.initialValues, ...cloneDeep(this.search) }
-    }
+        if ('datetimeRange' in temp) {
+          temp.startTime = temp.datetimeRange[0] ? moment(temp.datetimeRange[0]).format('YYYYMMDDHHmm') : ''
+          temp.endTime = temp.datetimeRange[1] ? moment(temp.datetimeRange[1]).format('YYYYMMDDHHmm') : ''
 
-    // 监听搜索表单的值的变化，与 store 做同步。以便其他组件执行表格查询时的参数统一
-    this.$watch(
-      () => this.form.getFieldsValue(),
-      async value => {
-        await this.$store.commit('setState', {
-          value: this.transformValue(value),
+          temp = omit(temp, 'datetimeRange')
+        }
+
+        if ('monthRange' in temp) {
+          temp.appointmentDateStartMonth = temp.monthRange[0] ? moment(temp.monthRange[0]).format('YYYYMM') : ''
+          temp.appointmentDateEndMonth = temp.monthRange[1] ? moment(temp.monthRange[1]).format('YYYYMM') : ''
+
+          temp = omit(temp, 'monthRange')
+        }
+
+        return temp
+      },
+      async onClear() {
+        this.form.resetFields()
+        await this.onSubmit(null, this.params, this.options)
+      },
+      async onSearch(payload, options) {
+        await this.$store.dispatch('setSearch', {
           moduleName: this.moduleName,
           submoduleName: this.submoduleName,
-          stateName: 'search',
-          merge: true
+          isResetSelectedRows: true,
+          additionalQueryParameters: {
+            ...this.$route.query,
+            // 获取子模块数据需要的额外参数，在引用该混合的子模块内覆盖设置。
+            // 请根据参数的取值和性质自行决定在 data 内或 computed 内定义。
+            ...(this.additionalQueryParameters || {})
+          },
+          isFetchList,
+          ...options,
+          payload
         })
-      }
-    )
+      },
+      /**
+       * 查询
+       * @param [e] {Event}
+       * @param [params] {Object} 其他自定义参数
+       * @param [options] {Object} 配置
+       */
+      onSubmit(e, params, options) {
+        e?.preventDefault()
 
-    this.search = this.initialValues
-  },
-  mounted() {
-    if (!isInitializeFromStore) {
-      this.search = this.convertBoolean(cloneDeep(this.form.getFieldsValue()))
-    }
-  },
-  methods: {
-    convertBoolean(value) {
-      const temp = {}
-
-      Object.entries(value).forEach(([k, v]) => {
-        if (isBoolean(v)) {
-          temp[k] = v ? 1 : 0
+        if (typeof setParams === 'function') {
+          this.params = {
+            ...params,
+            ...setParams.call(this)
+          }
         } else {
-          temp[k] = v
+          this.params = params
         }
-      }, {})
 
-      return temp
-    },
-    /**
-     * 此函数值保留一些高频共用类参数的处理
-     * @param values
-     * @returns {{}}
-     */
-    transformValue(values) {
-      let temp = this.convertBoolean(cloneDeep(values))
-
-      if ('dateRange' in temp) {
-        temp.startTime = temp.dateRange[0] ? moment(temp.dateRange[0]).format('YYYYMMDD') : ''
-        temp.endTime = temp.dateRange[1] ? moment(temp.dateRange[1]).format('YYYYMMDD') : ''
-
-        temp = omit(temp, 'dateRange')
-      }
-
-      if ('datetimeRange' in temp) {
-        temp.startTime = temp.datetimeRange[0] ? moment(temp.datetimeRange[0]).format('YYYYMMDDHHmm') : ''
-        temp.endTime = temp.datetimeRange[1] ? moment(temp.datetimeRange[1]).format('YYYYMMDDHHmm') : ''
-
-        temp = omit(temp, 'datetimeRange')
-      }
-
-      if ('monthRange' in temp) {
-        temp.appointmentDateStartMonth = temp.monthRange[0] ? moment(temp.monthRange[0]).format('YYYYMM') : ''
-        temp.appointmentDateEndMonth = temp.monthRange[1] ? moment(temp.monthRange[1]).format('YYYYMM') : ''
-
-        temp = omit(temp, 'monthRange')
-      }
-
-      return temp
-    },
-    async onClear() {
-      this.form.resetFields()
-      await this.onSubmit(null, this.params, this.options)
-    },
-    async onSearch(payload, options) {
-      await this.$store.dispatch('setSearch', {
-        moduleName: this.moduleName,
-        submoduleName: this.submoduleName,
-        isResetSelectedRows: true,
-        additionalQueryParameters: {
-          ...this.$route.query,
-          // 获取子模块数据需要的额外参数，在引用该混合的子模块内覆盖设置。
-          // 请根据参数的取值和性质自行决定在 data 内或 computed 内定义。
-          ...(this.additionalQueryParameters || {})
-        },
-        isFetchList,
-        ...options,
-        payload
-      })
-    },
-    /**
-     * 查询
-     * @param [e] {Event}
-     * @param [params] {Object} 其他自定义参数
-     * @param [options] {Object} 配置
-     */
-    onSubmit(e, params, options) {
-      e?.preventDefault()
-
-      this.params = params
-      this.options = options
-
-      this.form.validateFields(async (err, values) => {
-        if (!err) {
-          const payload = this.transformValue(values)
-
-          await this.onSearch({ ...payload, ...params }, options)
+        if (typeof setOptions === 'function') {
+          this.options = {
+            ...options,
+            ...setOptions.call(this)
+          }
+        } else {
+          this.options = options
         }
-      })
+
+        this.form.validateFields(async (err, values) => {
+          if (!err) {
+            const payload = this.transformValue(values)
+
+            await this.onSearch({ ...payload, ...this.params }, this.options)
+          }
+        })
+      },
+      onTreeFold() {
+        this.treeCollapsed = !this.treeCollapsed
+      }
     },
-    onTreeFold() {
-      this.treeCollapsed = !this.treeCollapsed
+    render() {
+      return (
+        <Form
+          layout="inline"
+          onSubmit={this.onSubmit}
+          colon={false}
+          class={`tg-inquiry${this.treeCollapsed ? ' tg-inquiry-side-toggle-reverse' : ''}`}
+        >
+          {this.content}
+        </Form>
+      )
     }
-  },
-  render() {
-    return (
-      <Form
-        layout="inline"
-        onSubmit={this.onSubmit}
-        colon={false}
-        class={`tg-inquiry${this.treeCollapsed ? ' tg-inquiry-side-toggle-reverse' : ''}`}
-      >
-        {this.content}
-      </Form>
-    )
   }
-})
+}
