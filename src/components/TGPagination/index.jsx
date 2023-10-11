5d7dc0e84eb55817a10ebf1fa94edada89f0e007
Omsber <xyzsyx@163.com>
2023-10-11 16:11:24 +0800

    refactor: 优化组件

--- src/components/TGPagination/index.jsx
+++ src/components/TGPagination/index.jsx
@@ -1,19 +1,10 @@
 import './index.scss'
 import { Pagination } from 'ant-design-vue'
 import forPagination from '@/mixins/forPagination'
-import { omit } from 'lodash'
 
 export default {
   mixins: [forPagination],
   render() {
-    const attributes = {
-      props: omit({
-        ...this.paginationProps,
-        ...this.pagination
-      }, ['pageIndex']),
-      on: this.paginationOn
-    }
-
-    return <Pagination class="tg-pagination" {...attributes} />
+    return <Pagination class="tg-pagination" {...this.attributes} />
   }
 }
