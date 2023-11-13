b2d8efec8993b1e7690a27e28a9ada8e8972d3d9
Omsber <xyzsyx@163.com>
2023-11-13 17:24:08 +0800

    refactor: 优化无访问权限页面的逻辑

--- src/views/NoAccess.jsx
+++ src/views/NoAccess.jsx
@@ -5,12 +5,22 @@ export default {
   name: 'NoAccess',
   render() {
     return (
-      <Layout class={'tg-not-found'}>
+      <Layout class={'tg-not-found no-access'}>
         <TGHeader layout="manager" page={'not-found'} />
         <Layout class={'tg-not-found-content'}>
           <Space class={'hint'}>
             <span>无访问权限~</span>
-            <Button type={'link'} onClick={() => this.$router.replace('/')}>返回首页</Button>
+            {
+              +this.$route.query['no-link'] !== 1 &&
+              (
+                <Button
+                  type={'link'}
+                  onClick={() => this.$router.replace('/')}
+                >
+                  返回首页
+                </Button>
+              )
+            }
           </Space>
         </Layout>
       </Layout>
