8af2fad052761f201c74cf3d48d498bf9d91bd3a
Omsber <xyzsyx@163.com>
2023-11-14 15:46:25 +0800

    feat(TGLoginWithToken): 新增组件：通过携带 TOKEN 的方式登录

---
+++ src/components/TGLoginWithToken/index.jsx
@@ -0,0 +1,46 @@
+/**
+ * 通过第三方token登录
+ */
+
+import { message } from 'ant-design-vue'
+import { getCookie } from '@/utils/cookie'
+
+export default {
+  data() {
+    return { token: '' }
+  },
+  created() {
+    const searchToken = new URL(window.location.href).searchParams.get('token')
+
+    this.token = searchToken ||
+      this.$route.query.token ||
+      getCookie('token') ||
+      localStorage.getItem('token')
+
+    // 如果 search 中存在 token，则删除之
+    if (searchToken) {
+      window.history.replaceState(null, null, window.location.pathname)
+    }
+  },
+  async mounted() {
+    if (this.token) {
+      const response = await this.$store.dispatch('login/getUserInfo', { token: this.token })
+
+      if (!response) {
+        message.error('获取用户信息失败', 0)
+
+        await this.$store.dispatch('login/clear')
+        this.$emit('errorStateChange', { status: true, error: new Error('获取用户信息失败') })
+      } else {
+        await this.$store.dispatch('login/jump')
+        this.$emit('errorStateChange', { status: false, error: null })
+      }
+    } else {
+      message.error('获取权限失败', 0)
+      this.$emit('errorStateChange', { status: true, error: new Error('请检查TOKEN是否有效') })
+    }
+  },
+  render() {
+    return null
+  }
+}
