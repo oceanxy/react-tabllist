da776b5b192cee60f54d215e6bb83010257a16fd
Omsber <xyzsyx@163.com>
2023-12-18 15:27:52 +0800

    fix: 移除在子项目的config内合并全局配置的代码，改为在全局配置的导出文件内合并，且改为深合并

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 import config from './config'
+import { merge } from 'lodash'
 
 // 读取子系统的配置文件，
 // 如果子系统不存在配置文件，则取全局配置文件（src/config/config）为项目的配置文件，
+// 如果两个文件同时存在，则深合并两个配置文件
 
 // APP_CONFIG 来自 webpack 全局注入的当前子系统配置文件
-export default Object.assign(config, APP_CONFIG)
+export default merge(config, APP_CONFIG)
