cd370074912b40987a96e14d56d9379f5cae030f
Omsber <xyzsyx@163.com>
2023-12-21 18:07:51 +0800

    refactor: 优化主文件

--- src/assets/styles/index.js
+++ src/assets/styles/index.js
@@ -7,50 +7,46 @@
 
 import './themeFromLess.scss'
 import { getFirstLetterOfEachWordOfAppName } from '@/utils/utilityFunction'
-import { join, resolve } from 'path'
 
-const { NODE_ENV, VUE_APP_PUBLIC_PATH } = process.env
 const appName = getFirstLetterOfEachWordOfAppName()
 
-function loadStyle(url) {
+export function loadStyle(url) {
   const link = document.createElement('link')
 
   link.type = 'text/css'
   link.rel = 'stylesheet'
   link.href = url
 
-  document.head.appendChild(link)
+  const head = document.getElementsByTagName('head')[0]
+
+  head.appendChild(link)
 }
 
-function loadScript(url) {
+export function loadScript(url) {
   const script = document.createElement('script')
 
   script.type = 'text/javascript'
   script.src = url
 
-  document.head.appendChild(script)
+  document.body.appendChild(script)
 }
 
-/**
- * 加载主题（生产环境和开发环境因为webpack打包机制的不同，采用不同的方式实现）
- * @param config
- * @param store
- */
-export default function loadVariablesStyle(config, store) {
-  const theme = store?.state?.login?.userInfo?.themeFileName ||
-    localStorage.getItem(`${appName}-theme`) ||
-    config.theme.default
+export function loadVariablesStyle(config, store) {
+  let _theme = localStorage.getItem(`${appName}-theme`)
 
-  if (NODE_ENV !== 'production') {
-    window.themeVariables = require(`./themes/${theme}/index.less`)
-  } else {
-    fetch(resolve(join(__dirname, VUE_APP_PUBLIC_PATH, '/manifest.json')))
-      .then(response => response.json())
-      .then(async data => {
-        const theme = localStorage.getItem(`${appName}-theme`) || config.theme.default
+  if (!_theme) {
+    _theme = config.theme.default
+    localStorage.setItem(`${appName}-theme`, _theme)
+  }
 
-        loadStyle(resolve(join(__dirname, `${VUE_APP_PUBLIC_PATH}/${data[`${theme}.css`]}`)))
-        loadScript(resolve(join(__dirname, `${VUE_APP_PUBLIC_PATH}/${data[`${theme}.js`]}`)))
-      })
+  // 加载主题
+  const theme = store?.state?.login?.userInfo?.themeFileName || _theme
+
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      window.themeVariables = require(`./themes/${theme}/index.less`)
+    } catch (e) {
+      window.themeVariables = require(`./themes/${config.theme.default}/index.less`)
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import '@/assets/styles/app.scss'
 import Vue from 'vue'
 import config from './config'
+import useComponents from '@/utils/antvComponents'
 import router from './router'
 import store from './store'
-import loadVariablesStyle from '@/assets/styles'
-import useComponents from '@/utils/antvComponents'
+import { loadScript, loadStyle, loadVariablesStyle } from '@/assets/styles'
+import { join, resolve } from 'path'
+import { getFirstLetterOfEachWordOfAppName } from '@/utils/utilityFunction'
 
-// 加载主题
-loadVariablesStyle(config, store)
+const appName = getFirstLetterOfEachWordOfAppName()
 
-// 注册组件
 useComponents(config)
 
 Vue.config.productionTip = false
@@ -19,6 +19,22 @@ Vue.prototype.$config = config
 // 预载mock数据（开发环境下并启用mock时执行）
 if (process.env.NODE_ENV === 'development' && config.mock) {
   require('../mock/index.js')
+}
+
+const { NODE_ENV, VUE_APP_PUBLIC_PATH } = process.env
+
+// 加载主题（生产环境和开发环境因为webpack打包机制的不同，所以采用不同的方式实现）
+if (NODE_ENV === 'production') {
+  fetch(resolve(join(__dirname, VUE_APP_PUBLIC_PATH, '/manifest.json')))
+    .then(response => response.json())
+    .then(async data => {
+      const theme = localStorage.getItem(`${appName}-theme`) || config.theme.default
+
+      loadStyle(resolve(join(__dirname, `${VUE_APP_PUBLIC_PATH}/${data[`${theme}.css`]}`)))
+      loadScript(resolve(join(__dirname, `${VUE_APP_PUBLIC_PATH}/${data[`${theme}.js`]}`)))
+    })
+} else {
+  loadVariablesStyle(config, store)
 }
 
 new Vue({
